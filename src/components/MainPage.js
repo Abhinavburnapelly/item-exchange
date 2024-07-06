9e6a23749fdb6d17714b5f45bd542ac5c7ce1230
Abhinavburnapelly <burnapellyabhinav@gmail.com>
2024-07-06 21:07:10 +0530

    adding buttons to the website

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,18 +1,36 @@
-import React from 'react'
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Form from 'react-bootstrap/Form';
-// import Navbar from 'react-bootstrap/Navbar';
-import '../App.css'
-import HeadNav from './HeadNav'
-
+import React from 'react';
+import '../App.css';
+import HeadNav from './HeadNav';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 function MainPage() {
   return (
     <div>
-      <HeadNav/>
+      <HeadNav />
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+            <Button 
+              className="main-button" 
+              variant="light" 
+              href="/add-item" 
+              style={{ width: '100%', height: '100px' }}>
+              Add Item to Store
+            </Button>
+          </Col>
+          <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+            <Button 
+              className="main-button" 
+              variant="light" 
+              href="/buy-item" 
+              style={{ width: '100%', height: '100px' }}>
+              Buy Item
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
